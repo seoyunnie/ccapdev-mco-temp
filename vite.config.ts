2da10b6b638b44d38ebe9f6d1172a4ae50a5ee29
Seoyun <seoyun1062@outlook.com>
2026-02-16 18:29:32 +0800

    Remove path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -8,5 +7,4 @@ export default defineConfig({
     tanstackRouter({ target: "react", generatedRouteTree: "./src/route-tree.gen.ts", autoCodeSplitting: true }),
     react({ babel: { plugins: ["babel-plugin-react-compiler"] } }),
   ],
-  resolve: { alias: { "@": path.resolve(import.meta.dirname, "./src") } },
 });
