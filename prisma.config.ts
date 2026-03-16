64202e77407367bd482bcfacd3ec39ee1ae65429
Seoyun <seoyun1062@outlook.com>
2026-03-17 05:14:58 +0800

    Move remaining Prisma configs to config file

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: { path: "prisma/migrations" },
+  migrations: { path: "prisma/migrations", seed: "tsx ./prisma/seed.ts" },
   engine: "classic",
   datasource: { url: env("DATABASE_URL") },
 });
